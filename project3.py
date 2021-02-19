bfa63bb09ea6d2794d5a31695a270a6679134da5
jebidiah252 <gordo.fjeldsted@aggiemail.usu.edu>
2021-02-18 21:25:33 -0700

    Changing to Ted Cruz

--- project3.py
+++ project3.py
@@ -14,6 +14,24 @@ accessTokenSecret = 'udzdwTP1ljEbD0HJwIFzkUIBCpq0VVSCx9ewJxoyWptvL'
 
 twitter = Twython(app_key=apiKey, app_secret=apiSecret)
 
+statuses = twitter.search(q='"Ted Cruz", lang:en', tweet_mode='extended', count=5000)['statuses']
+for status in statuses:
+    if 'retweeted_status' in status:
+        status['full_text'] = status["retweeted_status"]['full_text']
+
+
+#%%
+texts = [status['full_text'] for status in statuses]
+
+# Legacy cards
+cruz = [text.lower().count('cruz') for text in texts]
+cancun = [text.lower().count('cancun') for text in texts]
+texas = [text.lower().count('texas') for text in texts]
+winter = [text.lower().count('winter') for text in texts]
+d = {'Cruz':cruz, 'Cancun':cancun, 'Texas':texas, 'winter': winter}
+df = pd.DataFrame(data=d)
+
+df.head(50)
 # search = twitter.search(q='"Danaerys", #GameOfThrones, since:2011-04-17', tweet_mode='extended', count=5000)
 # search = twitter.search(q='"Dani", love OR hate, #GameOfThrones, lang:en, until:2021-01-01, since:2011-04-17, -filter:links, -filter:replies')
 
