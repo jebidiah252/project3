5ef406deb9db72697bea77aa71289310249421a7
Gordon Fjeldsted <gordon.fjeldsted@gmail.com>
2021-02-18 20:03:10 -0700

    trying out twitter api

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ This is the repo for project 3 group 6.
 The question that we will be asking is:
 
 What was Twitter's Change of Perception of Danaerys Targaryen between Season One and the Final Season of Game of Thrones?
+
+nltk
+
+"Dani" (love OR hate) (#GameOfThrones) lang:en until:2021-01-01 since:2011-04-17 -filter:links -filter:replies
--- project3.py
+++ project3.py
@@ -3,5 +3,18 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
+from twython import Twython
 
-print('Hello World')
+apiKey = 'ISPrLuKZP3BhMjZ990qkKLA36'
+apiSecret = 'feAaCueYAn0VYpqtK9YTu8RjCApsp25ABLwXjv3DTqcMKPvtGs'
+bearerToken = 'AAAAAAAAAAAAAAAAAAAAADJiMwEAAAAASrtDxf6dOzIQpTd6m5kYbpb0aUI%3DbMcbJ078aEgDspPWybWjQOdZotz5aztxtIkmIHCR5hYfapmFgD'
+
+accessToken = '1417014738-JWmhcvHtNUBoHVNORu9ptoilzH0KoAs5loypVBD'
+accessTokenSecret = 'udzdwTP1ljEbD0HJwIFzkUIBCpq0VVSCx9ewJxoyWptvL'
+
+twitter = Twython(app_key=apiKey, app_secret=apiSecret)
+
+# search = twitter.search(q='"Danaerys", #GameOfThrones, since:2011-04-17', tweet_mode='extended', count=5000)
+# search = twitter.search(q='"Dani", love OR hate, #GameOfThrones, lang:en, until:2021-01-01, since:2011-04-17, -filter:links, -filter:replies')
+
+# print(search)
