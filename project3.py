f34995d7c7aa1107a226a0b9fc8a2f0e0b3fdff3
ilikeike-cmd <51969989+ilikeike-cmd@users.noreply.github.com>
2021-02-21 10:27:58 -0700

    added most popular hashtag by day in posts mentioning cruz

--- project3.py
+++ project3.py
@@ -37,25 +37,25 @@ cruz = [text.lower().count('cruz') for text in texts]
 cancun = [text.lower().count('cancun') for text in texts]
 texas = [text.lower().count('texas') for text in texts]
 winter = [text.lower().count('winter') for text in texts]
-d = {'Cruz':cruz, 'Cancun':cancun, 'Texas':texas, 'winter': winter}
-df = pd.DataFrame(data=d)
+dhash = {'Cruz':cruz, 'Cancun':cancun, 'Texas':texas, 'winter': winter}
+#df = pd.DataFrame(data=d)
 
-df.head(50)
+#df.head(50)
 # search = twitter.search(q='"Danaerys", #GameOfThrones, since:2011-04-17', tweet_mode='extended', count=5000)
 # search = twitter.search(q='"Dani", love OR hate, #GameOfThrones, lang:en, until:2021-01-01, since:2011-04-17, -filter:links, -filter:replies')
 
 # print(search)
 
 #%%
-d = defaultdict(int)
+dhash = defaultdict(int)
 
 
-def getoneweekago():
-    return (datetime.today() - timedelta(6)).strftime('%Y-%m-%d')
+def getdaysago(days):
+    return (datetime.today() - timedelta(days)).strftime('%Y-%m-%d')
 
 
 def gethashtags(searchterm):
-    statuses = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=100, until=getoneweekago())[
+    statuses = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=100, until=getdaysago(6))[
         'statuses']
     newid = statuses[len(statuses) - 1]['id']
     endofweekid = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=1)['statuses'][0]['id']
@@ -74,7 +74,7 @@ def gethashtags(searchterm):
         for status in statuses:
             if len(status['entities']['hashtags']) > 0:
                 for item in status['entities']['hashtags']:
-                    d[item['text']] += 1
+                    dhash[item['text']] += 1
 
         # gets the next set of statuses
         statuses = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=100, max_id=newid)['statuses']
@@ -85,13 +85,116 @@ def gethashtags(searchterm):
             break
 
 
-# get most popular hashtags by tweets including search term
-gethashtags('romney')
+dwords = defaultdict(int)
 
-lists = sorted(d.items(), key=lambda item: item[1], reverse=True)
-x, y = zip(*lists)  # unpack a list of pairs into two tuples
-x = x[:10]
-y = y[:10]
+
+def getpopwords(searchterm):
+    statuses = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=100, until=getdaysago(6))[
+        'statuses']
+    newid = statuses[len(statuses) - 1]['id']
+    endofweekid = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=1)['statuses'][0]['id']
+
+    # this will get a tally of how many times each unique hashtag was used
+    totalapirequests = 0
+    while newid < endofweekid:
+        print(statuses[0]['created_at'])
+
+        # fix retweets to not show username
+        for status in statuses:
+            if 'retweeted_status' in status:
+                status['full_text'] = status["retweeted_status"]['full_text']
+
+        # increment tweets in dicto based on num of appearances
+        for status in statuses:
+            text = status['full_text']
+            text = re.sub(r'[^\w]', ' ', text)
+            text = word_tokenize(text)
+            text = [word for word in text if not word in stopwords.words()]
+            for word in text:
+                dwords[word] += 1
+
+        # gets the next set of statuses
+        statuses = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=100, max_id=newid)['statuses']
+        newid = statuses[len(statuses) - 1]['id']
+        totalapirequests += 1
+        print("api requests:", totalapirequests)
+        if totalapirequests > 50:
+            break
+
+
+def gethashtagsbyday(searchterm):
+    # this will get a tally of how many times each unique hashtag was used
+    dlist = []
+    endofweekid = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=1)['statuses'][0]['id']
+    for i in range(6, 0, -1):
+        dhashbyday = defaultdict(int)
+        # increment day
+        newday = getdaysago(i)
+        statuses = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=100, until=newday)['statuses']
+        totalapirequests = 0
+        newid = statuses[len(statuses) - 1]['id']
+        while newid < endofweekid:
+            statuses = twitter.search(q=searchterm, tweet_mode='extended', lang='en', count=100, max_id=newid)[
+                'statuses']
+            newid = statuses[len(statuses) - 1]['id']
+            print(statuses[0]['created_at'])
+
+            # fix retweets to not show username
+            for status in statuses:
+                if 'retweeted_status' in status:
+                    status['full_text'] = status["retweeted_status"]['full_text']
+
+            # increment tweets in dicto based on num of appearances
+            for status in statuses:
+                if len(status['entities']['hashtags']) > 0:
+                    for item in status['entities']['hashtags']:
+                        dhashbyday[item['text']] += 1
+            totalapirequests += 1
+            if totalapirequests == 10:
+                break
+        dlist.append((newday[5:], dhashbyday))
+    return dlist
+
+
+# # get most popular hashtags by tweets including search term
+# gethashtags('cruz')
+#
+# lists = sorted(dhash.items(), key=lambda item: item[1], reverse=True)
+# x, y = zip(*lists)  # unpack a list of pairs into two tuples
+# x = x[:10]
+# y = y[:10]
+#
+# # display most popular 10 tweets over timeframe starting 6 days ago and ending when
+# # i run out of api requests
+# # 900 / 15 min or 100,000 for a day
+# plt.plot(x, y)
+# plt.show()
+
+##############################################################
+
+# # get most popular words in tweet by tweets including search term
+# getpopwords('cruz')
+#
+# lists = sorted(dwords.items(), key=lambda item: item[1], reverse=True)
+# x, y = zip(*lists)  # unpack a list of pairs into two tuples
+# x = x[:10]
+# y = y[:10]
+#
+# # display most popular 10 tweets over timeframe starting 6 days ago and ending when
+# # i run out of api requests
+# # 900 / 15 min or 100,000 for a day
+# plt.plot(x, y)
+# plt.show()
+
+# get most popular words in tweet by tweets including search term
+temp = gethashtagsbyday('cruz')
+
+megalist = []
+for day in temp:
+    megalist.append((day[0], (sorted(day[1].items(), key=lambda item: item[1], reverse=True)[0])))
+
+x = [str(i[0]) + ' ' + str(i[1][0]) for i in megalist]
+y = [i[1][1] for i in megalist]
 
 # display most popular 10 tweets over timeframe starting 6 days ago and ending when
 # i run out of api requests
